26e3ffb27f0c06901aad1873c4fa46fc2de83b1b
r2d2kion <3024563@edison.k12.nj.us>
2023-12-30 17:36:57 -0500

    Initial Commit

---
+++ src/first.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class first {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+        for (int i=1; i<=30;i= i+1){
+
+            for (int j=i; j<=30;j= j+1){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/study/getdata.java
@@ -0,0 +1,18 @@
+package study;
+
+import java.util.Scanner;
+
+public class getdata {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Name");
+        String name = sc.nextLine();
+        System.out.println(name);
+        System.out.println("Enter Number 1");
+        int a = sc.nextInt();
+        System.out.println(a);
+        System.out.println("---");
+
+
+    }
+}

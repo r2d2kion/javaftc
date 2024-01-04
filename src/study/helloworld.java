cc9bf8ddbe5716c885a061a8930575fbc8e3b47e
r2d2kion <3024563@edison.k12.nj.us>
2024-01-03 19:26:03 -0500

    cleancodehelloworld

--- src/study/helloworld.java
+++ src/study/helloworld.java
@@ -4,16 +4,15 @@ import java.util.Scanner;
 
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("please enter a text");
+        System.out.println("helloworld");
+        System.out.println("enter a text");
         Scanner sc = new Scanner(System.in);
         String a = sc.nextLine();
-        System.out.println("entered text is " + a);
-        System.out.println("please enter a number");
+        System.out.println("enter a number");
         int b = sc.nextInt();
-        System.out.println("entered number is " + b);
-        System.out.println("please enter a boolean - true or false");
-        boolean c = sc.nextBoolean();
-        System.out.println("entered boolean is " + c);
+        for (int i=1; i <= b; i++) {
+            System.out.println(a);
+        }
     }
 }
+

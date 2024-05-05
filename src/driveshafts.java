d22d3fcd46ed93b47472d80ea808cdb4700135bf
KrishnaKamal <3024563@edison.k12.nj.us>
2024-05-04 20:00:54 -0400

    Create driveshafts.java

---
+++ src/driveshafts.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class driveshafts {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("enter a text");
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println("enter a number");
+        int b = sc.nextInt();
+        }
+    }
